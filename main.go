62fb28664174f25ed5ef8474f208421ea3fd6681
fengya <zhangfengya@yunlaba.com>
2017-06-29 17:56:46 +0800

    first commit

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	_ "beepro/routers"
 	"fmt"
 	"log"
 	"os"
@@ -13,5 +12,9 @@ func main() {
 	beego.BConfig.Listen.EnableAdmin = true
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	log.SetPrefix(fmt.Sprintf("PID.%d ", os.Getpid()))
+	f, _ := os.OpenFile("logs/log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	defer f.Close()
+	log.SetOutput(f)
+	beego.SetLogger("file", `{"filename":"logs/log.log"}`)
 	beego.Run()
 }
