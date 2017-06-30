f3dd29b19e8cb081da6412b5b2687fea29c31d6e
fengya <zhangfengya@yunlaba.com>
2017-06-30 16:58:13 +0800

    change package

--- controllers/default.go
+++ controllers/default.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"io"
 	"log"
+	"os"
+	"time"
 
 	"github.com/astaxie/beego"
 )
@@ -36,13 +39,17 @@ func (c *FormController) Get() {
 	c.TplName = "add_form.html"
 }
 func (c *FormController) Post() {
-	name := c.GetString("name")
-	log.Println(name)
-	if name != "" {
-		c.TplName = "amaui.html"
-	} else {
-		c.Ctx.WriteString("数据有误")
-	}
+	p := c.Ctx.Request.Form
+	log.Println(p)
+	timestamp := time.Now().Unix()
+	tm := time.Unix(timestamp, 0)
+	timer := tm.Format("2006-01-02 15:04:05")
+	f, _ := os.OpenFile("logs/user.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	data := "参赛单位:" + p["unit"][0] + "\n" + "参赛作品名称:" + p["pro_name"][0] + "\n" + "联系人电话:" + p["tel"][0] + "\n" + "联系人姓名:" + p["user"][0] + "\n" + "邮箱:" + p["email"][0] + "\n" + "通讯地址:" + p["address"][0] + "\n"
+
+	io.WriteString(f, "\n【"+timer+"】\n==================================\n"+data)
+	defer f.Close()
+	c.TplName = "pay.html"
 	// c.Ctx.WriteString("成功提交数据")
 	// c.Redirect("/home", 302)
 	// return
@@ -58,8 +65,26 @@ func (this *DownloadController) Get() {
 func (this *DownloadController) Page() {
 	params := this.Ctx.Input.Params()
 	log.Println(params["0"])
-	this.TplName = "download.html"
+	if params["0"] == "" {
+		this.TplName = "download.html"
+	} else {
+		this.Ctx.Output.Download("download/" + params["0"])
+	}
 }
 func (this *BillController) Show() {
-	this.TplName = "bill.html"
+	p := this.Ctx.Request.Form
+	log.Println(p)
+	if this.Ctx.Request.Method == "POST" {
+		timestamp := time.Now().Unix()
+		tm := time.Unix(timestamp, 0)
+		timer := tm.Format("2006-01-02 15:04:05")
+		f, _ := os.OpenFile("logs/ticket.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		data := "参赛单位:" + p["unit"][0] + "\n" + "纳税人识别码:" + p["code"][0] + "\n" + "单位地址:" + p["address"][0] + "\n" + "单位电话:" + p["tel"][0] + "\n" + "开户银行:" + p["bank_name"][0] + "\n" + "开户银行账号:" + p["bank"][0] + "\n"
+
+		io.WriteString(f, "\n【"+timer+"】\n==================================\n"+data)
+		defer f.Close()
+		this.Ctx.WriteString("申请提交成功")
+	} else {
+		this.TplName = "bill.html"
+	}
 }
--- main.go
+++ main.go
@@ -13,9 +13,12 @@ func main() {
 	beego.BConfig.Listen.EnableAdmin = true
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	log.SetPrefix(fmt.Sprintf("PID.%d ", os.Getpid()))
-	f, _ := os.OpenFile("logs/log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	defer f.Close()
-	log.SetOutput(f)
+	// f, _ := os.OpenFile("logs/log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	// defer f.Close()
+	// log.SetOutput(f)
 	beego.SetLogger("file", `{"filename":"logs/log.log"}`)
+	// beego.BConfig.Log.Outputs["console"] = ""
+	// logs.SetLogger("file", `{"filename":"test.log","maxlines":1000,"maxsize":10240}`)
+	// logs.SetLevel(logs.LevelInformational)
 	beego.Run()
 }
